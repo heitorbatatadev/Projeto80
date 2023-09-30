1f181a06331142b8064027057078ec006164cde1
heitorbatatadev <129177617+heitorbatatadev@users.noreply.github.com>
2023-09-30 09:31:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var GuestName = document.getElementById("name1").value;
+namesOfPeople.push(GuestName)
+
+namesOfPeople.sort();
+
+function searching(){
+
+    var s= documnet.getElementById(s1).value
+    var found=0
+    var  j:
+    for (j = 0; j <namesOfPeople.length; j++) {
+        if (s==namesOfPeople [j]){
+
+            found=found+1
+
+        }
+        
+        document.getElementById("p2").innerHTML="Nome encontrado" "+found+" (vez"es");
+        console.log("Nome encontrado" "+found+" (vez"es"));
+
+        
+    }
+}
